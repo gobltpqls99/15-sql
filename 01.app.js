3dffa09aac1f79a18c8125e0222474bf38d5c111
gobltpqls99 <gobltpqls99@naver.com>
2021-01-22 11:26:18 +0900

    ..

--- 01.app.js
+++ 01.app.js
@@ -1,31 +1,23 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 const moment = require('moment');
-
 const { mysql, connection } = require('./modules/mysql-conn');
 
-const app = express();
+const cityRouter = require('./routes/city-route');
 
 app.listen(3000, () => { console.log('http://127.0.0.1:3000'); });
-
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.locals.pretty = true;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/city', cityRouter);
 
 
-app.get('/city', (req, res) => {
-	const pug ={
-		file: 'city',
-	}
-	// 도시 리스트
-	res.render('/city/list', pug);
-});
-app.get('/city/create', (req, res) => {
-	// 도시 등록
-	res.send('/city/create');
-});
 app.use((req, res) => {
 	res.send('/404');
 });
