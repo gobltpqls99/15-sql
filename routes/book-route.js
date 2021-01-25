a07bb5cc17c08c11a64890f576ab0bf4c0ad4d87
gobltpqls99 <gobltpqls99@naver.com>
2021-01-25 14:23:54 +0900

    ..

---
+++ routes/book-route.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const { pool } = require('../modules/mysql-pool'); 
+const pugs = { file: 'book', title: '도서등록시스템' }
+
+router.get('/', (req, res, next) => {
+	res.send('리스트');
+});
+
+router.get('/create', (req, res, next) => {
+	const pug = { ...pugs };
+	res.render('book/create', pug);
+});
+
+router.post('/save', async (req, res, next) => {
+	try {
+		const sql = 'INSERT INTO books SET title=?, writer=?, wdate=?';
+		const value = [req.body.title, req.body.writer, new Date()];
+		const r = await pool.query(sql, value);
+		res.redirect('/book');
+	}
+	catch(e) {
+		const error = {
+			title: 'SQL Error',
+			code: 500,
+			msg: e.message
+		}
+		next(error);
+	}
+});
+
+module.exports = router;
